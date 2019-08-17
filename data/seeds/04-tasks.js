f5508190cc7b0a1484a35c5acd0efdcbe371983b
ChristianMacDonald <chrismacd412@gmail.com>
2019-08-17 13:59:40 -0400

    Added completed values for task seeds

--- data/seeds/04-tasks.js
+++ data/seeds/04-tasks.js
@@ -5,11 +5,11 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('tasks').insert([
-        { project_id: 1, description: 'Decide whether to buy a pre-built PC or build one.', notes: 'Building a PC is more time consuming but saves you money.' },
-        { project_id: 1, description: 'Decide your budget.', notes: 'A high end gaming PC can easily cost more than $1,000, but you don\'t have to spend that much if your willing to play at lower quality.' },
-        { project_id: 2, description: 'Keep your attendance consistent.', notes: 'Lambda School kicks you out if you miss more than 8 hours of class in a month.' },
-        { project_id: 2, description: 'If your falling behind, ask for help.', notes: 'Being too stubborn to ask for help will waste hours of your time.' },
-        { project_id: 3, description: 'Decide if you really care that much about the show.' }
+        { project_id: 1, description: 'Decide whether to buy a pre-built PC or build one.', notes: 'Building a PC is more time consuming but saves you money.', completed: 1 },
+        { project_id: 1, description: 'Decide your budget.', notes: 'A high end gaming PC can easily cost more than $1,000, but you don\'t have to spend that much if your willing to play at lower quality.', completed: 1 },
+        { project_id: 2, description: 'Keep your attendance consistent.', notes: 'Lambda School kicks you out if you miss more than 8 hours of class in a month.', completed: 0 },
+        { project_id: 2, description: 'If your falling behind, ask for help.', notes: 'Being too stubborn to ask for help will waste hours of your time.', completed: 0 },
+        { project_id: 3, description: 'Decide if you really care that much about the show.', completed: 0 }
       ]);
     });
 };
